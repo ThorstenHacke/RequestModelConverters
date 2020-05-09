03ba31e1811bd5ed4d0d68d266eb7ce80f913a71
Thorsten Hacke <thorsten_hacke@web.de>
2020-05-09 21:11:19 +0200

    Added Exaple to Create a simple converter from a model using Newtonsoft.Json Attributes to a query sting

---
+++ RequestModelConverters/RequestModelConvertersExtension.Tests/Examples/Json/Model.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RequestModelConvertersExtension.Tests.Examples.Json
+{
+    public class Model
+    {
+        [JsonProperty]
+        public string ValueOne { get; set; } = "ValueOne";
+    }
+
+    public class ModelComplexOne
+    {
+        [JsonProperty("Hallo")]
+        [JsonConverter(typeof(JsonDateTimeConverter))]
+        public DateTime ValueOne { get; set; } = new DateTime(2020,05,10);
+
+        public static string ExpectedValue = "?Hallo=2020-05-10";
+    }
+
+    public class BadModel
+    {
+        [JsonProperty("Hallo")]
+        [JsonConverter(typeof(string))]//This is not a Converter
+        public DateTime ValueOne { get; set; } = new DateTime(2020, 05, 10);
+
+        public static string ExpectedValue = "?Hallo=2020-05-10";
+    }
+
+    public class JsonDateTimeConverter : IsoDateTimeConverter
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="JsonDateTimeConverter"/>
+        /// </summary>
+        public JsonDateTimeConverter()
+        {
+            base.DateTimeFormat = "yyyy-MM-dd";
+        }
+    }
+
+}
